32d0c21564aed3502a0422b5a545c52038248256
Miquel <abellagarciamiquel@gmail.com>
2023-04-06 11:48:52 +0200

    fixed bug in wine recommendations

--- src/Components/Pages/RecipeDetails/Recommendation/index.jsx
+++ src/Components/Pages/RecipeDetails/Recommendation/index.jsx
@@ -1,5 +1,6 @@
 export const Recommendation = ({ data }) => {
-  if (!data.winePairing.pairingText.length) return;
+    console.log(data)
+  if (!data.winePairing.pairingText) return;
   return (
     <div className="flex items-center justify-center pb-20">
       <div className="mx-4 md:mx-0 md:w-4/5 text-gray-900 text-lg bg-green-300 p-12">
